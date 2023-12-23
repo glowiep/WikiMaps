6f0e74a74c3d0da2b2bdf15d4c23007c4d2eb169
svaronc <sebastianvaron96@gmail.com>
2023-12-23 16:18:26 +0000

    change schema for the database

--- db/schema/06_contributions.sql
+++ db/schema/06_contributions.sql
@@ -4,6 +4,4 @@ CREATE TABLE "contributions" (
   id SERIAL PRIMARY KEY,
   "users_id" INTEGER REFERENCES user(id),
   "map_id" INTEGER REFERENCES maps(id),
-  point_modified_date TIMESTAMP REFERENCES points(modified_date),
-  point_creation_date TIMESTAMP REFERENCES points(creation_date)
 );
