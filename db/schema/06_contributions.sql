9803e8186176ca1dede91c5c329cb9662f0e3e0b
svaronc <sebastianvaron96@gmail.com>
2023-12-23 17:08:26 +0000

    changes db schema and start routes

--- db/schema/06_contributions.sql
+++ db/schema/06_contributions.sql
@@ -2,8 +2,7 @@ DROP TABLE IF EXISTS contributions CASCADE;
 
 CREATE TABLE "contributions" (
   id SERIAL PRIMARY KEY,
-  "users_id" INTEGER REFERENCES user(id),
+  "users_id" INTEGER REFERENCES users(id),
   "map_id" INTEGER REFERENCES maps(id),
-  point_modified_date TIMESTAMP REFERENCES points(modified_date),
-  point_creation_date TIMESTAMP REFERENCES points(creation_date)
+  points_id INTEGER REFERENCES points(id)
 );
