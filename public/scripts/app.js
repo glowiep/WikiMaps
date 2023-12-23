2bedbcded5eddad65a5a75fd7891307bc2e40808
Gloria Lim <glorialimartt@gmail.com>
2023-12-22 22:47:38 -0500

    Add zoom control, scale bar and search bar to map

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,30 @@
 // Client facing scripts here
 $(document).ready(function() {
-  const map = L.map('map').setView([51.505, -0.09], 13);
+  // This setup the leafmap object by linking the map() method to the map id (in <section> html element)
+  const map = L.map('map').setView([43.644218, -79.402229], 13);
 
-  // Add tileLayer
+  // Add OpenStreetMap tileLayer
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
+    zoom: 1.5,
+    minZoom: 2,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
+
+  //add zoom control with your options
+  map.zoomControl.setPosition('topright');
+  
+  // Add scale bar
+  L.control.scale().addTo(map);
+
+  // Add search bar
+  const searchControl = new L.esri.Controls.Geosearch().addTo(map);
+  const results = new Location.LayerGroup().addTo(map);
+  searchControl.on('results', function(data) {
+    results.clearLayers();
+    for (let i = data.results.length - 1; i >= 0; i--) {
+      results.addLayer(L.marker(data.results[i].latlng));
+    }
+  })
 
 })
