a08f3a190b295a06db333c503746509a8f6e5376
Gloria Lim <glorialimartt@gmail.com>
2023-12-29 18:26:59 -0500

    Fix quotation marks

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,11 +1,11 @@
 -- Users table seeds here (Example)
-INSERT INTO users (username, email, password, profile_info) 
-VALUES ("guest", "guest@guest.com", "password", "This is a guest account.");
-INSERT INTO users (username, email, password, profile_info) 
-VALUES ("svaronc", "svaronc@mail.com", "password", "This is Sebastian Varon's account");
-INSERT INTO users (username, email, password, profile_info) 
-VALUES ("glowiep", "glowiep@mail.com", "password", "This is Gloria Lim's account");
-INSERT INTO users (username, email, password, profile_info) 
-VALUES ("user1", "user1@mail.com", "password", "This is user1");
-INSERT INTO users (username, email, password) 
-VALUES ("user1", "user1@mail.com", "password");
+INSERT INTO users (id, username, email, password, profile_info) 
+VALUES (1, 'guest', 'guest@guest.com', 'password', 'This is a guest account.');
+INSERT INTO users (id, username, email, password, profile_info) 
+VALUES (2, 'svaronc', 'svaronc@mail.com', 'password', 'This is Sebastian Varon account');
+INSERT INTO users (id, username, email, password, profile_info) 
+VALUES (3, 'glowiep', 'glowiep@mail.com', 'password', 'This is Gloria Lim account');
+INSERT INTO users (id, username, email, password, profile_info) 
+VALUES (4, 'user1', 'user1@mail.com', 'password', 'This is user1');
+INSERT INTO users (id, username, email, password, profile_info) 
+VALUES (5, 'user1', 'user1@mail.com', 'password', '');
--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,4 +1,4 @@
 INSERT INTO maps (title, description, creator_id) 
-VALUES ("Map 1", "User1 map", 2);
+VALUES ('Map 1', 'User1 map', 4);
 INSERT INTO maps (title, description, private, creator_id) 
-VALUES ("Map 2", "User2 map", TRUE, 3);
+VALUES ('Map 2', 'User2 map', TRUE, 5);
