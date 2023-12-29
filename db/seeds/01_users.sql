6e32460d2a8e51cf45deed21b1a3ba73ebeebdbc
Gloria Lim <glorialimartt@gmail.com>
2023-12-29 18:02:45 -0500

    Seeds initial commit

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,11 @@
 -- Users table seeds here (Example)
-INSERT INTO users (username) VALUES ('Alice');
-INSERT INTO users (username) VALUES ('Kira');
+INSERT INTO users (username, email, password, profile_info) 
+VALUES ("guest", "guest@guest.com", "password", "This is a guest account.");
+INSERT INTO users (username, email, password, profile_info) 
+VALUES ("svaronc", "svaronc@mail.com", "password", "This is Sebastian Varon's account");
+INSERT INTO users (username, email, password, profile_info) 
+VALUES ("glowiep", "glowiep@mail.com", "password", "This is Gloria Lim's account");
+INSERT INTO users (username, email, password, profile_info) 
+VALUES ("user1", "user1@mail.com", "password", "This is user1");
+INSERT INTO users (username, email, password) 
+VALUES ("user1", "user1@mail.com", "password");
