faceb67fe3e2c04b582cec66d13f147d1a72f65f
svaronc <sebastianvaron96@gmail.com>
2023-12-30 19:34:24 +0000

    fix the toolbar and searchbox

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -29,67 +29,93 @@ $(document).ready(function () {
 
   // Empty layer group to store results --newly added
   let results = L.layerGroup().addTo(map);
-  
-//removeThisLater: test to show coordinates on console
-console.log(results); 
 
+  //removeThisLater: test to show coordinates on console
+  console.log(results);
 
   // create the geocoding control and add it to the map --newly added
   const searchControl = L.esri.Geocoding.geosearch({
     useMapBounds: true,
     expanded: false,
     zoomToResult: true,
-    position: 'topleft',
+    position: "topleft",
     collapseAfterResult: true,
-    placeholder: 'Search Address'
+    placeholder: "Search Address",
   }).addTo(map);
 
-// removeThisLater: test to show coordinates in console
-  map.on('click', 
-					function(e){
-						var coord = e.latlng.toString().split(',');
-						var lat = coord[0].split('(');
-						var lng = coord[1].split(')');
-						console.log("You clicked the map at latitude: " + lat[1] + " and longitude:" + lng[0]);
-					});
-
-window.removeMarker = function(index) {
-    map.removeLayer(markers[index].marker);
-    markers.splice(index, 1);
-    updateMarkerList();
-};
+  // removeThisLater: test to show coordinates in console
+  map.on("click", function (e) {
+    var coord = e.latlng.toString().split(",");
+    var lat = coord[0].split("(");
+    var lng = coord[1].split(")");
+    console.log(
+      "You clicked the map at latitude: " + lat[1] + " and longitude:" + lng[0]
+    );
+  });
 
   // Listen for the results event and add marker to the map  --example - we can add a clear all button to clear the points from the map
-  searchControl.on("results", function(data) {
+  searchControl.on("results", function (data) {
     console.log(data.results);
     for (let i = data.results.length - 1; i >= 0; i--) {
       description = data.results[i].properties.LongLabel;
       longitude = data.results[i].properties.DisplayX;
       latitude = data.results[i].properties.DisplayY;
-      
-      markers = L.marker([latitude, longitude]).addTo(results)
+
+      markers = L.marker([latitude, longitude]).addTo(results);
     }
   });
 
-map.on('contextmenu', function(event) {
+  function updateMarkerList() {
+    let list = document
+      .getElementById("marker-list")
+      .getElementsByTagName("ul")[0];
+    list.innerHTML = "";
+    markers.forEach(function (markerObj, index) {
+      let listItem = document.createElement("li");
+      listItem.innerHTML =
+        markerObj.description +
+        ' <button onclick="removeMarker(' +
+        index +
+        ')">Delete</button>';
+      listItem.onclick = function () {
+        map.setView(markerObj.marker.getLatLng(), 13); // Centrar el mapa en el marcador
+        markerObj.marker.openPopup(); // Abrir el popup del marcador
+      };
+      list.appendChild(listItem);
+    });
+  }
+  window.removeMarker = function (index) {
+    map.removeLayer(markers[index].marker);
+    markers.splice(index, 1);
+    updateMarkerList();
+  };
+  map.on("contextmenu", function (event) {
     tempLatLng = event.latlng;
-    document.getElementById('markerModal').style.display = 'block';
-});
+    document.getElementById("markerModal").style.display = "block";
+  });
 
-window.addMarker = function() {
-    let description = document.getElementById('description').value;
-    let imageUrl = document.getElementById('image').value;
+  window.addMarker = function () {
+    let description = document.getElementById("description").value;
+    let imageUrl = document.getElementById("image").value;
     let marker = L.marker([tempLatLng.lat, tempLatLng.lng]).addTo(map);
 
-    marker.bindPopup('<b>Description:</b> ' + description + '<br><img src="' + imageUrl + '" alt="imagen" style="width:100%;">').openPopup();
+    marker
+      .bindPopup(
+        "<b>Description:</b> " +
+          description +
+          '<br><img src="' +
+          imageUrl +
+          '" alt="imagen" style="width:100%;">'
+      )
+      .openPopup();
 
     markers.push({ marker: marker, description: description }); // Guardar el marcador y la descripción
 
     updateMarkerList();
-    document.getElementById('markerModal').style.display = 'none';
-    document.getElementById('description').value = '';
-    document.getElementById('image').value = '';
-};
+    document.getElementById("markerModal").style.display = "none";
+    document.getElementById("description").value = "";
+    document.getElementById("image").value = "";
+  };
   // (geoman) define Drawing toolbar options
   var options = {
     position: "topleft", // toolbar position, options are 'topleft', 'topright', 'bottomleft', 'bottomright'
