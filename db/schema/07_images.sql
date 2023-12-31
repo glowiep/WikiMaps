4ceab25208230cf19e2664dd3196e464b981e6ca
Gloria Lim <glorialimartt@gmail.com>
2023-12-30 23:03:05 -0500

    Update seed data

--- db/schema/07_images.sql
+++ db/schema/07_images.sql
@@ -2,8 +2,7 @@ DROP TABLE IF EXISTS images CASCADE;
 
 CREATE TABLE "images" (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255),
+  -- name VARCHAR(255),
   images_url VARCHAR(255),
   points_id INTEGER REFERENCES points(id) ON DELETE CASCADE
 );
--- stretch
--- db/seeds/06_contributions.sql
+++ db/seeds/06_contributions.sql
@@ -1,2 +1,2 @@
 INSERT INTO contributions (id, user_id, map_id, points_id) 
-VALUES (1, 2, 2, 5); -- svaronc contributed one point to the Vancouver map
+VALUES (1, 4, 2, 5); -- HappyMapper contributed one point to the Vancouver map
