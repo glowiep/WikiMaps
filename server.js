dd8a42ea7c39a6b139ba8fd4e8bea58d28e59d28
Gloria Lim <glorialimartt@gmail.com>
2023-12-31 22:57:13 -0500

    Add route for GET /maps/:username/:user_id/:map_id

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.static('public'));
 // Note: Feel free to replace the example routes below with your own
 const userApiRoutes = require('./routes/users-api');
 const mapListApiRoutes = require('./routes/map-list-api');
-// const mapApiRoutes = require('./routes/map-api');
+const mapApiRoutes = require('./routes/map-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
 
@@ -39,6 +39,7 @@ const usersRoutes = require('./routes/users');
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
 app.use('/api/users', userApiRoutes);
 app.use('/maps/:username/api/maps', mapListApiRoutes);  //for logged in user
+app.use('/maps/:username/:user_id', mapApiRoutes);  //for logged in user
 app.use('/api/maps', mapListApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
