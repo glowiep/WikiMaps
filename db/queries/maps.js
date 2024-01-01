60fdbe1c692d9023a12d65daa76ac551087f1149
Gloria Lim <glorialimartt@gmail.com>
2024-01-01 14:18:47 -0500

    Add getUserContributions query

--- db/queries/maps.js
+++ db/queries/maps.js
@@ -11,9 +11,9 @@ const getPublicMaps = () => {
     });
 };
 
-// Get maps associated with a given user
-const getUserMaps = (user_id) => {
-  return db.query('SELECT * FROM maps WHERE creator_id = $1', [user_id])
+// Get points associated with a given map ID
+const getMapPoints = (map_id) => {
+  return db.query('SELECT * FROM points WHERE map_id = $1', [map_id])
     .then(data => {
       return data.rows[0];
     })
@@ -22,9 +22,10 @@ const getUserMaps = (user_id) => {
     });
 };
 
-// Get points associated with a given map ID
-const getMapPoints = (map_id) => {
-  return db.query('SELECT * FROM points WHERE map_id = $1', [map_id])
+
+// Get maps associated with a given user to be displayed on Profile tab
+const getUserMaps = (user_id) => {
+  return db.query('SELECT * FROM maps WHERE creator_id = $1', [user_id])
     .then(data => {
       return data.rows[0];
     })
@@ -38,7 +39,7 @@ const getUserFavourites = (user_id) => {
   return db.query(`
   SELECT DISTINCT maps.id as maps_id, maps.title as map_title, maps.description 
   FROM favorites JOIN maps ON favorites.map_id = maps.id
-  WHERE favorites.user_id = 4
+  WHERE favorites.user_id = $1
   AND maps.private = FALSE;
   `, [user_id])
     .then(data => {
@@ -49,4 +50,21 @@ const getUserFavourites = (user_id) => {
     });
 };
 
-module.exports = { getPublicMaps, getUserMaps, getMapPoints, getUserFavourites };
+// Get maps user contributed to, to be displayed on Profile tab
+const getUserContributions = (user_id) => {
+  return db.query(`
+    SELECT maps.id as map_id, maps.title as maps_title, maps.description, maps.creator_id, users.username as creator_username 
+    FROM contributions 
+    JOIN maps ON contributions.map_id = maps.id
+    JOIN users ON maps.creator_id = users.id
+    WHERE contributions.user_id = $1;
+  `, [user_id])
+    .then(data => {
+      return data.rows[0];
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = { getPublicMaps, getUserMaps, getMapPoints, getUserFavourites, getUserContributions };
--- db/seeds/06_contributions.sql
+++ db/seeds/06_contributions.sql
@@ -1,2 +1,2 @@
 INSERT INTO contributions (id, user_id, map_id, points_id) 
-VALUES (1, 4, 2, 5); -- HappyMapper contributed one point to the Vancouver map
+VALUES (1, 4, 2, 5); -- HappyMapper contributed one point to the Vancouver map (map_id = 2)
