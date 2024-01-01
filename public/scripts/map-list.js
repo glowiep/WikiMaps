e7d244277d891e1d22aaf4252836042b4a387f96
Gloria Lim <glorialimartt@gmail.com>
2024-01-01 15:29:26 -0500

    Modify appended html

--- public/scripts/map-list.js
+++ public/scripts/map-list.js
@@ -15,11 +15,9 @@ $(() => {
       for (const map of response.maps) {  // eventually link to http://localhost:8080/api/maps/<map_id>
         // $mapList.append(`<li class="list"><a href='#'>` + map.title + `</a></li>`);
         $mapList.append(`
-      <div id="map-list">
         <div class="card">
           <i class="fa-solid fa-magnifying-glass"></i> <a class="map-list-item" href="#"><b>` + map.title + `</b></a>
         </div>
-      </div>
       `)
         // $mapList.append(`<li class="list"><a href='http://` + process.env.DB_HOST + `:`+ process.env.DB_PORT + `/api/maps/` + map.id + `>` + map.title + `</a></li>`);
         // $(`<li class="list">`).text(map.title).appendTo($mapList);
