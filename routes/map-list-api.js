29cb05946d76601e3251b9872bd4dd01265531a2
Gloria Lim <glorialimartt@gmail.com>
2024-01-01 14:47:44 -0500

    Modify comments

--- routes/map-list-api.js
+++ routes/map-list-api.js
@@ -1,7 +1,7 @@
 /*
  * All routes for Discover tab data are defined here
- * Since this file is loaded in server.js into /maps,
- *   these routes are mounted onto /maps
+ * Since this file is loaded in server.js into /maps/:username/api/maps (logged in user) and /api/maps (guest),
+ *   these routes are mounted onto /maps/:username/api/maps and /api/maps
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
@@ -9,7 +9,7 @@ const express = require('express');
 const router  = express.Router();
 const mapListQueries = require('../db/queries/maps');
 
-// GET /api/maps/list
+// GET /api/maps/list (guest) and GET /maps/:username/api/maps/list (logged in user)
 router.get('/list', (req, res) => {
   mapListQueries.getPublicMaps()
     .then(maps => {
