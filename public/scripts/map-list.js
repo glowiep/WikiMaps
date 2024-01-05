b82eba3c1f0f6f40505cab3d146f84806ee08a85
Gloria Lim <glorialimartt@gmail.com>
2024-01-04 19:17:11 -0500

    Add id to div element.

--- public/scripts/map-list.js
+++ public/scripts/map-list.js
@@ -14,7 +14,7 @@ $(() => {
       // Append new list items based on API response
       for (const map of response.maps) {  // eventually link to http://localhost:8080/api/maps/<map_id>
         $mapList.append(`
-        <div class="card">
+        <div class="card" id=map${map.id}>
         <a class="map-card" href="#">  
           <i class="fa-solid fa-magnifying-glass action-item"></i> 
           <div class="map-card"><b> ${map.title} </b></div>
