03e45f2bce605ee703c7adc21ad1caf1494c745c
Gloria Lim <glorialimartt@gmail.com>
2024-01-06 15:32:26 -0500

    Load points list for guest

--- public/scripts/map-list.js
+++ public/scripts/map-list.js
@@ -1,5 +1,46 @@
-// This loads the list of public maps in the Discover tab (for guest)
+// For guest sidebar
 $(() => {
+  $("#discover-tab").on("click", ".discover", function(e) {
+    e.preventDefault();
+    const map_id = $(this).attr('id');
+    loadPoints(map_id);
+  });
+  /**
+   * Function to load points based on the map_id, to display on the view tab
+   * GET /maps/:user_id/points
+   */
+  function loadPoints(map_id) {
+    $.ajax({
+      url: `/maps/:user_id/${map_id}/points`,
+      type: "GET",
+      success: function (points) {
+        console.log(points);
+        const $pointList = $('#point-list')
+        // Clear existing list items
+        $pointList.empty();
+
+        // Append point list items based on API response
+        $.each(points, function (index, point) {
+          $pointList.append(`
+            <div class="point-item" id=${point.id}>
+              <div>📍 ${point.description} </div>
+              <div class="point-actions">
+                <button class="icon-button view-point-button">
+                  <span><i class="fa-solid fa-eye action-item"></i></span>
+                </button>
+              </div>
+            </div>
+          `)
+        });
+      },
+      error: function (xhr, status, error) {
+        console.error("Error:", error);
+      },
+    });
+  }
+
+
+  // This loads the list of public maps in the Discover tab (for guest)
   const $fetchMapList = function() {
     $.ajax({
       url: 'api/maps/list',
