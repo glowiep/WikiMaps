129abff1574e303d9062fad61e9524b1c5554280
Gloria Lim <glorialimartt@gmail.com>
2024-01-06 03:01:13 -0500

    Modify Add Point window style

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,11 @@
 // Client facing scripts here
 
 $(() => {
-  
-  
+  $("#closeModalBtn").on("click", function () {
+    closeMarkerModal();
+  });
+  function closeMarkerModal() {
+    var markerModal = $("#markerModal");
+    markerModal.hide();
+  }
 });
