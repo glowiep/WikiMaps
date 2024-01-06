7a7bec824b8532d21099ba96d1e50d2f105cb254
Gloria Lim <glorialimartt@gmail.com>
2024-01-06 12:41:04 -0500

    Add event handler to open map-tab when point-button is clicked

--- public/scripts/sidebar.js
+++ public/scripts/sidebar.js
@@ -5,7 +5,15 @@
 $(document).ready(function() {
   // Add sidebar
   const sidebar = L.control.sidebar('sidebar').addTo(map);
+
+  // New Map button opens New Map tab
   $("#new-map-button").click(function (e) {
     sidebar.open('map-tab');
   });
-})
+
+  // Add Point button opens New Map tab
+  $("#point-button").click(function (e) {
+    sidebar.open('map-tab');
+  });
+
+});
