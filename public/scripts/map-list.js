c026a4987ab9bcb07abd202fae6a7c75d76961ab
Gloria Lim <glorialimartt@gmail.com>
2024-01-08 18:31:26 -0500

    Fix guest page bug

--- public/scripts/map-list.js
+++ public/scripts/map-list.js
@@ -1,21 +1,22 @@
 // For guest sidebar
-import { loadMapInfo, loadPoints, fetchMapList } from "/scripts/helpers.js";
+import { loadDiscoverMapInfo, loadDiscoverPoints, fetchMapList } from "/scripts/helpers.js";
 $(() => {
-  // $("#map-list").on("click", ".card", function(e) {
-  //   e.preventDefault();
-  //   const map_id = $(this).attr('id');
-  //   loadMapInfo(map_id);
-  //   loadDiscoverPoints(map_id);
-  // });
+  $("#map-list").on("click", ".card", function(e) {
+    e.preventDefault();
+    const map_id = $(this).attr('id');
+    loadDiscoverMapInfo(map_id);
+    loadDiscoverPoints(map_id);
+    addPointsToMap(map_id);
+  });
   
   // Refresh view-tab point list when contributions are saved
-  // $("#view-tab").on("click", "#save-contribution", function(e) {
-  //   e.preventDefault();
-  //   const map_id = $("#view-tab").find(".map-title-info").attr('id');
-  //   setTimeout(() => {
-  //     loadPoints(map_id);
-  //   }, 230);
-  // });
+  $("#view-tab").on("click", "#save-contribution", function(e) {
+    e.preventDefault();
+    const map_id = $("#view-tab").find(".map-title-info").attr('id');
+    setTimeout(() => {
+      loadDiscoverPoints(map_id);
+    }, 230);
+  });
 
-  // fetchMapList();
+  fetchMapList();
 });
