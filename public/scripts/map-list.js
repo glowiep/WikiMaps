b835f581b9ec4d1db138572432f589780b46ae64
svaronc <sebastianvaron96@gmail.com>
2024-01-09 01:21:05 +0000

    import addPointsToMap

--- public/scripts/map-list.js
+++ public/scripts/map-list.js
@@ -1,22 +1,24 @@
 // For GUEST sidebar
-import { loadDiscoverMapInfo, 
-  loadGuestPoints, 
-  fetchMapList
+import {
+  loadDiscoverMapInfo,
+  loadGuestPoints,
+  fetchMapList,
+  addPointsToMap,
 } from "/scripts/helpers.js";
 
 $(() => {
-  $("#map-list").on("click", ".discover", function(e) {
+  $("#map-list").on("click", ".discover", function (e) {
     e.preventDefault();
-    const map_id = $(this).attr('id');
+    const map_id = $(this).attr("id");
     loadDiscoverMapInfo(map_id);
     loadGuestPoints(map_id);
     addPointsToMap(map_id);
   });
-  
+
   // Refresh view-tab point list when contributions are saved
-  $("#view-tab").on("click", "#save-contribution", function(e) {
+  $("#view-tab").on("click", "#save-contribution", function (e) {
     e.preventDefault();
-    const map_id = $("#view-tab").find(".map-title-info").attr('id');
+    const map_id = $("#view-tab").find(".map-title-info").attr("id");
     setTimeout(() => {
       loadGuestPoints(map_id);
     }, 230);
